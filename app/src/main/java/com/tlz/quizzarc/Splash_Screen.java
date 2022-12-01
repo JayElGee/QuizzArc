28941d3d205408ad7d2e6adc7cb0561f8331f726
TheLordZedd <pcgpod@yahoo.com>
2022-11-30 23:09:06 -0600

    Added login page, UI adjustments (in progress)

--- app/src/main/java/com/tlz/quizzarc/Splash_Screen.java
+++ app/src/main/java/com/tlz/quizzarc/Splash_Screen.java
@@ -31,7 +31,7 @@ public class Splash_Screen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(Splash_Screen.this, MainActivity.class);
+                Intent intent = new Intent(Splash_Screen.this, Login_Page.class);
                 startActivity(intent);
                 finish();
             }
