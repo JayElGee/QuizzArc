75b6fc057c8c7ab978b394d555ef06539c537ae1
TheLordZedd <pcgpod@yahoo.com>
2022-12-05 23:29:28 -0600

    Added sign in, sign out

--- app/src/main/java/com/tlz/quizzarc/Login_Page.java
+++ app/src/main/java/com/tlz/quizzarc/Login_Page.java
@@ -1,5 +1,6 @@
 package com.tlz.quizzarc;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,9 +8,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login_Page extends AppCompatActivity {
 
@@ -19,6 +27,9 @@ public class Login_Page extends AppCompatActivity {
     SignInButton signInGoogle;
     TextView signUp;
     TextView forgotPassword;
+    ProgressBar progressBarSignIn;
+
+    FirebaseAuth auth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +42,16 @@ public class Login_Page extends AppCompatActivity {
         signInGoogle = findViewById(R.id.signInButton);
         signUp = findViewById(R.id.tvLoginSignUp);
         forgotPassword = findViewById(R.id.tvLoginForgetPassword);
+        progressBarSignIn = findViewById(R.id.progressBarSignIn);
 
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                String userEmail = email.getText().toString();
+                String userPassword = password.getText().toString();
+
+                signInFirebase(userEmail, userPassword);
             }
         });
 
@@ -62,5 +78,40 @@ public class Login_Page extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void signInFirebase(String userEmail, String userPassword) {
+        progressBarSignIn.setVisibility(View.VISIBLE);
+        signIn.setClickable(false);
+
+        auth.signInWithEmailAndPassword(userEmail, userPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(Login_Page.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                            progressBarSignIn.setVisibility(View.INVISIBLE);
+                            Toast.makeText(Login_Page.this, "You have successfully signed in", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(Login_Page.this, "There was an issue with your sign in attempt", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser user = auth.getCurrentUser();
+
+        if (user != null) {
+            Intent intent = new Intent(Login_Page.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+            Toast.makeText(Login_Page.this, "Resuming Session", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/tlz/quizzarc/MainActivity.java
+++ app/src/main/java/com/tlz/quizzarc/MainActivity.java
@@ -2,9 +2,14 @@ package com.tlz.quizzarc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +17,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView signOut;
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        signOut = findViewById(R.id.tvSignOut);
+
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                auth.signOut();
+                Intent intent = new Intent(MainActivity.this, Login_Page.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
 
 
 
