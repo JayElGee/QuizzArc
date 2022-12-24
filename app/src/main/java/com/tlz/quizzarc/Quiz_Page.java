4eac7355b17f5a79cc5ff7e75c18fc4746d6abcd
TheLordZedd <pcgpod@yahoo.com>
2022-12-23 23:53:53 -0600

    Created Firebase database, beginning implementation

--- app/src/main/java/com/tlz/quizzarc/Quiz_Page.java
+++ app/src/main/java/com/tlz/quizzarc/Quiz_Page.java
@@ -7,11 +7,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Quiz_Page extends AppCompatActivity {
 
     TextView time, correct, wrong;
     TextView question, a, b, c, d;
     Button next, end;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference = database.getReference().child("Questions");
+
+    String quizQuestion;
+    String answerA;
+    String answerB;
+    String answerC;
+    String answerD;
+    String correctAnswer;
+    int questionCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +54,7 @@ public class Quiz_Page extends AppCompatActivity {
         end.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
             }
         });
 
