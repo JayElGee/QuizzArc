e36921973cfececab68afe1fa05e1a4560be7f60
TheLordZedd <pcgpod@yahoo.com>
2022-12-24 22:18:15 -0600

    Added UI responses to questions and answers

--- app/src/main/java/com/tlz/quizzarc/Quiz_Page.java
+++ app/src/main/java/com/tlz/quizzarc/Quiz_Page.java
@@ -1,14 +1,22 @@
 package com.tlz.quizzarc;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class Quiz_Page extends AppCompatActivity {
 
@@ -26,6 +34,12 @@ public class Quiz_Page extends AppCompatActivity {
     String answerD;
     String correctAnswer;
     int questionCount;
+    int questionNumber = 1;
+
+    String userAnswer;
+
+    int userCorrect = 0;
+    int userWrong = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,19 +58,148 @@ public class Quiz_Page extends AppCompatActivity {
         next = findViewById(R.id.buttonNextQuestion);
         end = findViewById(R.id.buttonEndGame);
 
+        game();
+
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                game();
             }
         });
 
-        end.setOnClickListener(new View.OnClickListener() {
+        a.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                userAnswer = "a";
 
+                if (correctAnswer.equals(userAnswer)) {
+                    a.setBackgroundColor(Color.GREEN);
+                    userCorrect++;
+                    correct.setText("" + userCorrect);
+                } else {
+                    a.setBackgroundColor(Color.RED);
+                    userWrong++;
+                    wrong.setText("" + userWrong);
+                    findAnswer();
+                }
             }
         });
 
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userAnswer = "b";
+
+                if (correctAnswer.equals(userAnswer)) {
+                    b.setBackgroundColor(Color.GREEN);
+                    userCorrect++;
+                    correct.setText("" + userCorrect);
+                } else {
+                    b.setBackgroundColor(Color.RED);
+                    userWrong++;
+                    wrong.setText("" + userWrong);
+                    findAnswer();
+                }
+            }
+        });
+
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userAnswer = "c";
+
+                if (correctAnswer.equals(userAnswer)) {
+                    c.setBackgroundColor(Color.GREEN);
+                    userCorrect++;
+                    correct.setText("" + userCorrect);
+                } else {
+                    c.setBackgroundColor(Color.RED);
+                    userWrong++;
+                    wrong.setText("" + userWrong);
+                    findAnswer();
+                }
+            }
+        });
+
+        d.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userAnswer = "d";
+
+                if (correctAnswer.equals(userAnswer)) {
+                    d.setBackgroundColor(Color.GREEN);
+                    userCorrect++;
+                    correct.setText("" + userCorrect);
+                } else {
+                    d.setBackgroundColor(Color.RED);
+                    userWrong++;
+                    wrong.setText("" + userWrong);
+                    findAnswer();
+                }
+            }
+        });
+
+
+    }
+
+    public void game() {
+        
+        a.setBackgroundColor(Color.WHITE);
+        b.setBackgroundColor(Color.WHITE);
+        c.setBackgroundColor(Color.WHITE);
+        d.setBackgroundColor(Color.WHITE);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                
+                questionCount = (int) dataSnapshot.getChildrenCount();
+                
+                quizQuestion = dataSnapshot.child(String.valueOf(questionNumber)).child("q").getValue().toString();
+                answerA = dataSnapshot.child(String.valueOf(questionNumber)).child("a").getValue().toString();
+                answerB = dataSnapshot.child(String.valueOf(questionNumber)).child("b").getValue().toString();
+                answerC = dataSnapshot.child(String.valueOf(questionNumber)).child("c").getValue().toString();
+                answerD = dataSnapshot.child(String.valueOf(questionNumber)).child("d").getValue().toString();
+                correctAnswer = dataSnapshot.child(String.valueOf(questionNumber)).child("answer").getValue().toString();
+
+                question.setText(quizQuestion);
+                a.setText(answerA);
+                b.setText(answerB);
+                c.setText(answerC);
+                d.setText(answerD);
+                
+                if (questionNumber < questionCount) {
+                    questionNumber++;
+                } else {
+                    Toast.makeText(Quiz_Page.this, "You have completed the quiz", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Toast.makeText(Quiz_Page.this, "An error has occurred", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void findAnswer() {
+        switch (correctAnswer) {
+            case "a":
+                a.setBackgroundColor(Color.GREEN);
+                break;
+            case "b":
+                b.setBackgroundColor(Color.GREEN);
+                break;
+            case "c":
+                c.setBackgroundColor(Color.GREEN);
+                break;
+            case "d":
+                d.setBackgroundColor(Color.GREEN);
+                break;
+        }
     }
 }
